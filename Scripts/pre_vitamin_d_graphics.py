0d549bf7120fd23b1b6b0b4a977b9d9e45f0eb4c
Giovanni <giovannilopez9808@gmail.com>
2021-01-07 07:06:52 -0600

    Change graphics colors and legend

--- Scripts/pre_vitamin_d_graphics.py
+++ Scripts/pre_vitamin_d_graphics.py
@@ -34,10 +34,10 @@ data_CIE_inv = np.loadtxt(
     dir_vitamin_d+"CIE_data.csv", delimiter=",", skiprows=inv+1, max_rows=91)*1.6/40
 x = np.arange(n_days)
 x2 = np.arange(91)
-plt.scatter(x, data_vitamin, c="#f72585", label="Modelo TUV", marker=".")
-plt.scatter(x, data_Herman, c="#3a0ca3", label="Herman", marker=".")
+plt.scatter(x, data_vitamin, c="#5158BB", label="Modelo TUV", marker=".")
+plt.scatter(x, data_Herman, c="#000000", label="Herman", marker=".")
 plt.scatter(x2[data_CIE_ver != 0]+ver, data_CIE_ver[data_CIE_ver != 0],
-            c="#33B62B", label="CIE-WMO", marker=".")
+            c="#33B62B", label="Coef. Prop.", marker=".")
 plt.scatter(x2[data_CIE_inv != 0]+inv, data_CIE_inv[data_CIE_inv != 0],
             c="#33B62B", marker=".")
 plt.ylabel("Irradiancia pre-vitamina D$_3$ (W/m$^2$)", fontsize=12)
@@ -50,7 +50,7 @@ plt.grid(ls="--", color="grey", alpha=0.3)
 plt.legend(frameon=False, ncol=3, markerscale=2)
 plt.subplots_adjust(left=0.117, bottom=0.138, right=0.93, top=0.957)
 plt.savefig(dir_graphics+"Previtamin_D.png", dpi=300)
-# plt.show()
+plt.show()
 
 data_Herman_ver = part_period(data_Herman, ver)
 data_vitamin_ver = part_period(data_vitamin, ver)
