c5d0fd9acf0a69cfac9dd5b8ae2b0898fc1262c8
Giovanni <giovannilopez9808@gmail.com>
2021-07-24 17:17:47 -0500

    Delete useless files and data

--- Data/Download_data/download_omi_500.sh
+++
@@ -1,1 +0,0 @@
-wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition -i data_list_omi_500.txt
--- Data/Download_data/download_terra.sh
+++
@@ -1,1 +0,0 @@
- wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=3  https://ladsweb.modaps.eosdis.nasa.gov/archive/orders/501516345/ --header "Authorization: Bearer Z2lvdmFubmlsb3Blejk4MDg6WjJsdmRtRnVibWxzYjNCbGVqazRNRGhBWjIxaGFXd3VZMjl0OjE2MDU1ODc4MzY6MTgxNzk2NGZhZDJlMTE5NzE5NDAyYmM2MTZmZWVkMGNmN2M3ZWZkMA" -P /
