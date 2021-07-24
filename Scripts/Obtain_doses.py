8280c7f8da4961ec1431ba5287322146f2728ab9
Giovanni <giovannilopez9808@gmail.com>
2021-07-24 16:21:31 -0500

    Change date format

--- Scripts/Obtain_doses.py
+++ Scripts/Obtain_doses.py
@@ -17,6 +17,17 @@ def calc_dosis(hour, data, lim):
     return time
 
 
+def obtain_date(file=""):
+    date = file.replace(".csv", "")
+    year = date[0:2]
+    month = date[2:4]
+    day = date[4:6]
+    date = "{}-{}-{}".format(year,
+                             month,
+                             day)
+    return date
+
+
 dir_data = "../PreVitamin_D/Rosario/"
 dir_results = "../Data/Rosario_period/"
 files = np.sort(listdir(dir_data))
@@ -25,9 +36,9 @@ dosis_med = 250
 time_vitamin = []
 time_med = []
 file_result = open(dir_results+"dosis_time.csv", "w")
-file_result.write("Dia,vitamin,MED\n")
+file_result.write("Date,vitamin,MED\n")
 for file in files:
-    print(file)
+    date = obtain_date(file)
     hour, uv_list, vitamin_list = np.loadtxt(dir_data+file,
                                              delimiter=",",
                                              skiprows=1,
@@ -35,5 +46,7 @@ for file in files:
                                              unpack=True)
     time_vitamin = calc_dosis(hour, vitamin_list, dosis_vitamin)
     time_med = calc_dosis(hour, uv_list/40, dosis_med)
-    file_result.write(file[0:6]+","+time_vitamin+","+time_med+"\n")
+    file_result.write("{},{},{}\n".format(date,
+                                          time_vitamin,
+                                          time_med))
 file_result.close()
