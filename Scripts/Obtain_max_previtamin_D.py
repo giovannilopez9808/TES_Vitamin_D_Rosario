1f17f2e54558c857ea3166bb57acd1eb35f6e542
Giovanni <giovannilopez9808@gmail.com>
2021-07-25 14:01:05 -0500

    Delete lines useless

--- Scripts/Obtain_max_previtamin_D.py
+++ Scripts/Obtain_max_previtamin_D.py
@@ -1,31 +1,12 @@
 from os import listdir
 import pandas as pd
-import numpy as np
-import datetime
-
-
-def date_yymmdd(day, day_i):
-    date = day_i+datetime.timedelta(days=day)
-    year = str(date.year)[2:4]
-    month = format_number(date.month)
-    day = format_number(date.day)
-    date = year+month+day
-    return date
-
-
-def format_number(number):
-    number_str = number
-    number_str = number_str.zfill(2)
-    return number_str
 
 
 parameters = {"path data": "../PreVitamin_D/Rosario/",
               "path results": "../Data/",
               "file results": "Max_pre_vitamin_D.csv",
-              "date initial": datetime.date(2019, 6, 1),
-              "date final": datetime.date(2020, 5, 31)
               }
-files = np.sort(listdir(parameters["path data"]))
+files = sorted(listdir(parameters["path data"]))
 file_result = open("{}{}".format(parameters["path results"],
                                  parameters["file results"]),
                    "w")
