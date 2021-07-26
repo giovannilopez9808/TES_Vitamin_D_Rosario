80ad956c2c3b5ce0f8beede977e4d4b175e6b4af
Giovanni <giovannilopez9808@gmail.com>
2021-07-25 20:11:04 -0500

    Add UVI Daily maximum

--- Scripts/Obtain_daily_tuv_data.py
+++ Scripts/Obtain_daily_tuv_data.py
@@ -10,13 +10,15 @@ files = sorted(listdir(parameters["path data"]))
 file_result = open("{}{}".format(parameters["path results"],
                                  parameters["file results"]),
                    "w")
-file_result.write("Date,SZA,Vitamin D\n")
+file_result.write("Date,SZA,UVI,Vitamin D\n")
 for file in files:
     data = pd.read_csv("{}{}".format(parameters["path data"],
                                      file))
+    uvi_max = data["UVI"].max()
     vitamin_max = data["Vitamin D"].max()
     sza_min = data["SZA"].min()
-    file_result.write("{},{},{}\n".format(file.replace(".csv", ""),
-                                          sza_min,
-                                          vitamin_max))
+    file_result.write("{},{},{},{}\n".format(file.replace(".csv", ""),
+                                             sza_min,
+                                             uvi_max,
+                                             vitamin_max))
 file_result.close()
