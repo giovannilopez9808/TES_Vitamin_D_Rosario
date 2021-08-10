362c2b08cc6d705ce007c5c166f7ca1f143e7238
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-08-10 13:55:58 -0500

    save the figure and print the three TES

--- Scripts/Graphics_doses.py
+++ Scripts/Graphics_doses.py
@@ -3,16 +3,26 @@ from functions import *
 import pandas as pd
 
 
+def obtain_filename_data(parameters: dict, ID: str):
+    filename = "{}{}.csv".format(parameters["file data"],
+                                 ID)
+    return filename
+
+
 parameters = {"path data": "../Data/",
               "path graphics": "../Graphics/",
-              "file data": "Doses_time.csv",
+              "file data": "Doses_time_RARG.csv",
               "graphics name": "dosis_vitamin.png",
               "date initial": "2019-06-01",
               "date final": "2020-06-01",
-              "Vitamin color": "#E7CA01",
+              "Vitamin color": "#f9c74f",
               "Vitamin label": "Dosis pre-vitamina D 136 J/m$^2$",
-              "MED color": "#fb8500",
-              "MED label": "Dosis eritémica mínima 250 J/m$^2$",
+              "1/4 MED color": "#f48c06",
+              "1/4 MED label": "Dosis eritémica mínima 62.5 J/m$^2$",
+              "1 MED color": "#dc2f02",
+              "1 MED label": "Dosis eritémica mínima 250 J/m$^2$",
+              "dataset": {"AOD": "0.30",
+                          "Ozone": "OMI"},
               }
 data = read_data(parameters["path data"],
                  parameters["file data"])
@@ -22,27 +32,36 @@ plt.plot(data.index, data["vitamin"],
          label=parameters["Vitamin label"],
          lw=1.5)
 plt.fill_between(data.index, data["vitamin"],
-                 data["MED"],
-                 color="#E7CA01",
+                 data["1/4 MED"],
+                 color="#faa307",
                  alpha=0.5)
-plt.plot(data.index, data["MED"],
-         color=parameters["MED color"],
-         label=parameters["MED label"],
+plt.plot(data.index, data["1/4 MED"],
+         color=parameters["1/4 MED color"],
+         label=parameters["1/4 MED label"],
          lw=1.5)
-plt.xlim(pd.to_datetime(parameters["date initial"]),
-         pd.to_datetime(parameters["date final"]))
+plt.fill_between(data.index, data["1/4 MED"],
+                 data["1 MED"],
+                 color="#e85d04",
+                 alpha=0.5)
+plt.plot(data.index, data["1 MED"],
+         color=parameters["1 MED color"],
+         label=parameters["1 MED label"],
+         lw=1.5)
 plt.xticks(months,
            months_names)
+plt.xlim(pd.to_datetime(parameters["date initial"]),
+         pd.to_datetime(parameters["date final"]))
 plt.xlabel("Periodo 2019-2020",
            fontsize=12)
 plt.ylim(0, 100)
 plt.yticks([tick for tick in range(0, 110, 10)])
 plt.ylabel("TES (minutos)",
            fontsize=12)
+# plt.title(title)
 plt.grid(ls="--",
          color="grey",
          alpha=0.5)
-plt.subplots_adjust(top=0.956,
+plt.subplots_adjust(top=0.917,
                     bottom=0.132,
                     left=0.106,
                     right=0.958,
@@ -50,7 +69,7 @@ plt.subplots_adjust(top=0.956,
                     wspace=0.2)
 plt.legend(frameon=False,
            fontsize=11)
-# plt.savefig("{}{}".format(parameters["path graphics"],
-#                           parameters["graphics name"]),
-#             dpi=400)
-plt.show()
+plt.tight_layout()
+plt.savefig("{}{}".format(parameters["path graphics"],
+                          parameters["graphics name"]),
+            dpi=400)
