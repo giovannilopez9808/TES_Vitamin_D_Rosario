0414476ecd1f8976fc6bbd6a9d6cc847393bbf66
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-08-15 01:26:34 -0500

    Add difference on seasons

--- Scripts/Obtain_mean_difference_between_TES.py
+++ Scripts/Obtain_mean_difference_between_TES.py
@@ -9,10 +9,20 @@ def obtain_RD(data: pd.DataFrame, datasets: dict):
     return data
 
 
+def print_results(mean: float, std: float):
+    print("Diferencia:\n\tMean: {:.2f}\n\tStd: {:.2f}".format(mean,
+                                                              std))
+
+
 parameters = {
     "path data": "../Data/",
     "file data": "Doses_time_",
     "city": "Rosario",
+    "seasons": {"winter": [pd.to_datetime("2019-06-21"),
+                           pd.to_datetime("2019-09-20")],
+                "summer": [pd.to_datetime("2019-12-21"),
+                           pd.to_datetime("2020-03-20")],
+                },
     "datasets": {"dataset1": "vitamin",
                  "dataset2": "1/4 MED"}
 }
@@ -25,5 +35,14 @@ data = obtain_RD(data,
                  parameters["datasets"])
 RD_mean = data["RD"].mean()
 RD_std = data["RD"].std()
-print("Diferencia:\n\tMean: {:.2f}\n\tStd: {:.2f}".format(RD_mean,
-                                                          RD_std))
+print_results(RD_mean, RD_std)
+for season in parameters["seasons"]:
+    dates = parameters["seasons"][season]
+    data_season = select_data_from_date_period(data,
+                                               dates[0],
+                                               dates[1])
+    RD_mean = data_season["RD"].mean()
+    RD_std = data_season["RD"].std()
+    print("-------------------------")
+    print(season)
+    print_results(RD_mean, RD_std)
